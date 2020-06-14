7541644c8d8e96490185cfa01eb5c29ebaaa99cd
Jacob Januszanis <Jjanuszanis94@gmail.com>
2020-06-14 13:51:41 -0600

    third commit

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 const express= require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html");
 })
+
 
 
 
